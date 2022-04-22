26472b0a4e3b2363ab168f6a9c039d8d28c7c272
hashimp7121 <70853597+hashimp7121@users.noreply.github.com>
2022-04-23 00:23:08 +0530

    Method added by developer-2

--- src/main/java/com/testwebservice/git_demo/App.java
+++ src/main/java/com/testwebservice/git_demo/App.java
@@ -6,6 +6,10 @@ public class App {
 			System.out.println("Success");
 		}
 	}
+	
+	public void addedMethod(){
+		System.out.println("new method");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
